f07c7622e611fc4945cad080c02465ec223c9e6a
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-06-30 23:28:39 -0300

    wip carMovement

---
+++ Prototype 1/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update() 
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 10, 0, Input.GetAxis("Vertical") * Time.deltaTime * 10);
+    }
+}
