a709f418c5ea108b863b1f9e4b1a7210fc1900b6
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-07-11 16:30:35 -0300

    wip

---
+++ Prototype 2/Assets/Scripts/DestroyOut.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOut : MonoBehaviour
+{
+    private float topBound = 30.0f;
+    private float lowerBound = -10.0f;
+
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float verticalInput;
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab;
 
     void Update()
     {
@@ -23,5 +23,10 @@ public class PlayerController : MonoBehaviour
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
