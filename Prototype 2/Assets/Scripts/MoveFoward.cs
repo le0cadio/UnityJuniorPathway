e46f5f67acff4ac7dfe9834e6a18dfe32bb0b8d1
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-07-12 00:08:35 -0300

    fix code

--- Prototype 2/Assets/Scripts/MoveFoward.cs
+++ Prototype 2/Assets/Scripts/MoveFoward.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class MoveFoward : MonoBehaviour
 {
    public float speed = 40.0f;
-
-    // Update is called once per frame
+   
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10.0f;
+    public float xRange = 13.0f;
     public GameObject projectilePrefab;
 
     void Update()
