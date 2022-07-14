117013d71cfb2a97c7b384adc7c6532cac16b4d1
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-07-14 17:51:24 -0300

    Wip prototype 3

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
