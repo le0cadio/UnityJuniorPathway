221535d79a5ce807967b111b1a9833e10e9c847f
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-07-18 19:58:53 -0300

    Change the road color and wip

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -20,11 +20,11 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !gameOver)
         {
+            playerAnim.SetTrigger("Jump_trig");
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
-            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
