b47ba33f780baf1ae989a29a279d79fa9637a783
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-07-19 11:06:33 -0300

    Update MoveLeft.cs

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -13,8 +13,6 @@ public class MoveLeft : MonoBehaviour
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-
-
     void Update()
     {
         if (playerControllerScript.gameOver == false)
