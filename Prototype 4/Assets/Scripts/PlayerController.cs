15dcef3c6ead796052b898db138fdb8721d803c1
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-07-20 06:19:32 -0300

    Wip prototype 4
    New Functionality:
    When the player collects a powerup, a visual indicator appears
    When the player collides with an enemy while they have the powerup, the enemy goes flying
    After a certain amount of time, the powerup ability and indicator disappear
    New Concepts and Skills:
    Debug concatenation
    Local component variables
    IEnumerators and WaitForSeconds()
    Coroutines
    SetActive(true/false)

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed = 5.0f;
+    private float powerUpStrength = 15.0f;
     private GameObject focalPoint;
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
 
     void Start()
     {
@@ -18,5 +21,37 @@ public class PlayerController : MonoBehaviour
     {
         float fowardInpulse = Input.GetAxis("Vertical");
         rb.AddForce(focalPoint.transform.forward * speed * fowardInpulse);
+        powerupIndicator.transform.position = transform.position new Vector3(0.0f, -0.5f, 0.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(2);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (
+                collision.gameObject.transform.position - transform.position
+            ).normalized;
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Player collided with enemy");
+        }
     }
 }
